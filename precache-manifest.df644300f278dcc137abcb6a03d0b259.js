867fb3ca0ebcab44c3ef0deccddecd1a338a9994
nhatnam9203 <nhatnam9203@gmail.com>
2019-01-23 19:50:35 +0700

    Updates

---
+++ precache-manifest.df644300f278dcc137abcb6a03d0b259.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28958b781c5fb0b690d6",
+    "url": "/ghichu/static/js/runtime~main.28958b78.js"
+  },
+  {
+    "revision": "71214cdbb801955ddfac",
+    "url": "/ghichu/static/js/main.71214cdb.chunk.js"
+  },
+  {
+    "revision": "4f7b6f35fb36798d2bd8",
+    "url": "/ghichu/static/js/1.4f7b6f35.chunk.js"
+  },
+  {
+    "revision": "71214cdbb801955ddfac",
+    "url": "/ghichu/static/css/main.6292f6c4.chunk.css"
+  },
+  {
+    "revision": "b1b2251a88e40ac35649704d18a37fe6",
+    "url": "/ghichu/index.html"
+  }
+];
